5d83581aacca90a1ec96bb40c9fe76a4a20a8622
unknown <you@example.com>
2022-11-06 10:53:28 -0800

    one

---
+++ 6-11/task1.js
@@ -0,0 +1,68 @@
+
+let birthdaymonth = prompt("Please enter your birthday", "1");
+
+switch (birthdaymonth) {
+
+  case "1":
+  month = "january";
+document.write("the input: " + birthdaymonth  + "<br>" + "the output:" + month )
+    break;
+
+  case "2":
+  month= "February";
+  document.write("the input: " + birthdaymonth  + "<br>" + "the output:" + month )
+    break;
+
+  case "3":
+  month = "March";
+  document.write("the input: " + birthdaymonth  + "<br>" + "the output:" + month )
+    break;
+
+  case "4":
+  month = "April";
+  document.write("the input: " + birthdaymonth  + "<br>" + "the output:" + month )
+    break;
+
+  case "5":
+  month = "May";
+  document.write("the input: " + birthdaymonth  + "<br>" + "the output:" + month )
+    break;
+
+  case "6":
+  month = "June";
+  document.write("the input: " + birthdaymonth  + "<br>" + "the output:" + month )
+    break;
+
+  case  "7":
+  month = "July";
+  document.write("the input: " + birthdaymonth  + "<br>" + "the output:" + month )
+  break;
+
+  case  "8":
+  month = "August";
+  document.write("the input: " + birthdaymonth  + "<br>" + "the output:" + month )
+  break;
+
+  case  "9":
+  month = "September";
+  document.write("the input: " + birthdaymonth  + "<br>" + "the output:" + month )
+  break;
+  case "10":
+
+  month = "October";
+  document.write("the input: " + birthdaymonth  + "<br>" + "the output:" + month )
+  break;
+
+  case  "11":
+  month = "";November
+  document.write("the input: " + birthdaymonth  + "<br>" + "the output:" + month )
+  break;
+
+case "12":
+  month = "December";
+  document.write("the input: " + birthdaymonth  + "<br>" + "the output:" + month )
+ break;
+
+ default :
+ document.write("wrong input")
+}
---
+++ 6-11/task2.js
@@ -0,0 +1,11 @@
+let a=1;
+for (let i = 1; i <= 4; i++) {
+  
+   for (let r = 1; r <= i; r++)
+  
+  {document.write(a + " ");
+   a++;
+  }
+
+  document.write("<br>");
+}
